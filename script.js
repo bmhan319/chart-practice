e74dc0a2731acfa0c10f707b7e48322db1d45d77
Brian Han <bmhan319@hotmail.com>
2018-07-30 20:14:51 -0700

    3rd bar chart added

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ google.charts.load('current', {'packages':['corechart']});
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
 google.charts.setOnLoadCallback(drawAllocationChart);
+google.charts.setOnLoadCallback(drawSemestersChart);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
@@ -56,5 +57,34 @@ function drawAllocationChart() {
 
   // Instantiate and draw our chart, passing in some options.
   var chart = new google.visualization.PieChart(document.getElementById('allocation'));
+  chart.draw(data, options);
+}
+
+function drawSemestersChart() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  var data = google.visualization.arrayToDataTable([
+        ["Semester", "Major Creits", { role: "style" } ],
+        ["09/18", 0, "#5180FF"],
+        ["01/19", 0, "#5180FF"],
+        ["09/19", 1, "#5180FF"],
+        ["01/20", 1, "#5180FF"],
+        ["09/20", 3, "#8FAEFF"],
+        ["01/21", 3, "#8FAEFF"],
+        ["09/21", 3, "#8FAEFF"],
+        ["01/22", 3, "#8FAEFF"]
+      ]);
+
+  // Set chart options
+  var options = {'title':'Major Studies Timeline',
+                 'width':400,
+                 'height':300,
+                 'pieHole': 0.4,
+                 'colors':['#28407F'],
+                };
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.BarChart(document.getElementById('semesters'));
   chart.draw(data, options);
 }
