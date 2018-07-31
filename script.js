99d74e5fad43114f808aee5e9a94e58e2da25e97
Brian Han <bmhan319@hotmail.com>
2018-07-30 18:59:27 -0700

    adjusted pie chart and added code into js file

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("let's build some charts!")
+
+
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 4],
+          ['Onions', 4],
+          ['Sausage', 4],
+          ['Peppers', 4],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Many Slices of My Pizza Last Night Had Specific Toppings',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
