f4edb8332c83a272813eff4752dc63597b598a9d
Brian Han <bmhan319@hotmail.com>
2018-07-30 19:19:28 -0700

    added a 2nd chart to the page

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ google.charts.load('current', {'packages':['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawAllocationChart);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
@@ -29,5 +30,27 @@ function drawChart() {
 
   // Instantiate and draw our chart, passing in some options.
   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
+
+function drawAllocationChart() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Score Ranges');
+  data.addColumn('number', 'Games');
+  data.addRows([
+    ['0-100', 2],
+    ['100-200', 5],
+    ['200-300', 3],
+  ]);
+
+  // Set chart options
+  var options = {'title':'My Bowling Scores From Last 10 Games',
+                 'width':400,
+                 'height':300};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('allocation'));
   chart.draw(data, options);
 }
